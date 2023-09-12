ff186c35c0d34d2b89b592a302f6456afaa49764
Kasleets <141558322+Kasleets@users.noreply.github.com>
2023-09-12 17:00:47 +0200

    The exercise should be finished, many things need to be further developed

--- GitHub Övning 1 CS/Program.cs
+++ GitHub Övning 1 CS/Program.cs
@@ -1,10 +1,101 @@
 ﻿namespace GitHub_Övning_1_CS
+
 {
-    internal class Program
+    using System;                                                               //adding libraries
+    using System.Collections.Generic;                                           //adding libraries
+    using System.IO;                                                            //trying to develop log file, didn't have enough time to finish it
+
+    public class Employee                                                       //class to create individual employees
     {
-        static void Main(string[] args)
+        public string Name { get; private set; }                                //public name so we can get it, used to store the values
+        public decimal Salary { get; private set; }                             //public salary so we can get it, used to store the values
+                                                                                //Had to switch to "private set" instead of "set" because the program didn't receive values properly
+        public Employee(string name, decimal salary)                            //creating an input that can be used as an 'index' for single employee with its name and salary
         {
-            Console.WriteLine("Hello, World!");
+            Name = name;                    //case sensitive variable
+            Salary = salary;                //case sensitive variable
         }
+        public override string ToString()                                       //converting the object to string so it's usable, otherwise it's displaying the program name instead of value, thank you Github...
+        {
+            return $"Namn: {Name}"+ 
+                   $"\nLön: {Salary}\n";
+        }
+        public class EmployeeRegister                                           //Creating Employee register class with a list 
+        {
+            private List<Employee> employees = new List<Employee>();            // TODO: creating our first list, need to figure out how to use it with the log file
+
+            public void AddEmployee(string name, decimal salary)                //adding an employee with name and salary, using void so it doesn't return
+            {                                                                   // TODO: Figure out how to set different formats in decimal if it is with comma or the dot in different systems and how to implement, better to have decimal points on salary rather than integer
+                var employee = new Employee(name, salary);                      //using var to define an employee so c# defines itself
+                employees.Add(employee);                                        //adding employee to the list
+
+            }
+            public void RegistryDisplay()                                       //method for writing the whole registry that saved data
+            {
+                Console.WriteLine("\nAnställda lista: ");                     // \n gives us a blank space in between
+
+                foreach (var employee in employees)                             //for the total amount of employess, write out the whole data set
+                {
+                    Console.WriteLine(employee);
+                }
+            }
+        }
+        public class Program                                                    //running a proper program to the user
+        {
+            // TODO: need to develop log file method and returning values, overwriting old data and making a backup, separate in new class or write 2 methods
+            /*trying to create a method for saving a log file with employees so it can backup the input data
+            public static void SaveToLog(EmployeeRegister register)
+            {
+
+            } */
+            static void Main(string[] args)                                     //regular main program
+            {
+                EmployeeRegister employeeRegister = new EmployeeRegister();     //Defining employee registry
+                                                                                // TODO: add a breakpoint to avoid memory leak in this place
+                while (true)                                                    //if using the registry give a choice to the user, infinite loop, 
+                {
+                    Console.WriteLine("\n1. Lägga till anställda");
+                    Console.WriteLine("2. Skriva ut alla anställda");
+                    Console.WriteLine("3. Exit");
+                    Console.Write("Välja funktion: ");                          //prompt to the user to choose option
+
+                    var choice = Console.ReadLine();                            //defining variable to read off the user input
+
+                    switch (choice)                                             //using method and class based on the user input
+                    {
+                        case "1":
+                            Console.WriteLine("Skriva anställda namn: ");
+                            var name = Console.ReadLine();                      //read user input for variable
+
+                            Console.WriteLine("Skriva anställda lön: ");
+                            var salary = decimal.Parse(Console.ReadLine());     //read user input for variable 
+                                                                                // TODO: Figure out how to handle the dot and comma exceptions in monetary systems... = convert everything to strings?
+
+                            employeeRegister.AddEmployee(name, salary);         //adding Employee to the list and memory, figure out the log file
+                            // TODO: figure out fixing the null value later
+                            //if salary == null Console.WriteLine("Ange korrekt lön"); try using " ? " operator to include null option
+                            break;
+
+                            case "2":
+                            employeeRegister.RegistryDisplay();                 //using the method to print the whole data set                       
+                            break;                                              // TODO: figure out if should close the program and how to implement the log file in it
+
+                            case "3":       // TODO: check why can't use Environment.Exit(0);
+                            return;
+
+                        default:                                                //in case user puts anything different like enter or 7, a failsafe
+                            Console.WriteLine("Ange rätt siffran, testa igen. ");
+                            break;
+                            
+                            /* try to fix null option
+                            if (choice == null) Console.WriteLine("Critical error, closing program");
+                            Environment.Exit(1);
+                            */
+                    }
+                }
+            }
+
+        }
+
     }
 }
---
+++ GitHub Övning 1 CS/README Uppgift och svar.txt
@@ -0,0 +1,43 @@
+﻿Bakgrund
+Ett litet företag i restaurangbranschen kontaktar dig för att utveckla ett litet
+personalregister. De har endast två krav:
+
+1. Registret skall kunna ta emot och lagra anställda med namn och lön. (via inmatning
+i konsolen, inget krav på persistent lagring)
+
+2. Programmet skall kunna skriva ut registret i en konsol.
+
+
+Uppgift 1
+Vilka klasser bör ingå i programmet?
+
+Vi måste ha minst 2 klasser.
+1 att representera individ Anställda
+2 att lagra ner Register och sen skriva ut den
+
+Bra att ha en log.txt fil, ska försöka göra den också
+
+
+Uppgift 2
+Vilka attribut och metoder bör ingå i dessa klasser?
+
+Vi behöver minst 2 metoder, 
+1 att skriva ut data
+2 att lagra data
+
+En attribut som vi behöver är egentligen lista av anställda såsom data lista.
+
+
+Uppgift 3
+Skriv programmet
+
+ligger under Program.cs
+
+
+
+Försök göra programmet så robust och framtidssäkert som möjligt!
+
+Bonus för att implementera test! (men inte på bekostnad av att den andra koden blir
+lidande)
+
+Ni har fram till 16.45 på er, koden ska ligga uppe absolut senast 17.00.
